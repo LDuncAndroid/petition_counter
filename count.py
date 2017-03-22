96507f80ddc1701701f4d31524515f8c9a83b0ab
Luke William Duncan <me@lukewilliamduncan.com>
2017-03-22 17:24:20 +0000

    printing counts by country

--- count.py
+++ count.py
@@ -14,17 +14,39 @@ def main():
 
     print('{} votes outside Great Britain'.format(outside_gb_count))
 
-    outside_scot_count = 0
+    scot_count = 0
+    eng_count = 0
+    wales_count = 0
+    ni_count = 0
+
+    country_codes = []
+
     for constituency in response.json()['data']['attributes']['signatures_by_constituency']:
-        # If the constituency is not in Scotland
-        if constituency['ons_code'][:1] != 'S':
-            outside_scot_count += constituency['signature_count']
+        country_code = constituency['ons_code'][:1]
+        constituency_count = constituency['signature_count']
 
-    print('{} votes outside Scotland'.format(outside_scot_count))
+        if country_code not in country_codes:
+            country_codes.append(country_code)
 
-    total_count_outside_scotland = outside_gb_count + outside_scot_count
+        if country_code == 'S':
+            scot_count += constituency_count
+        elif country_code == 'E':
+            eng_count += constituency_count
+        elif country_code == 'W':
+            wales_count += constituency_count
+        elif country_code == 'N':
+            ni_count += constituency_count
 
-    print('{} votes that don\'t matter to this petition'.format(total_count_outside_scotland))
+    total_signatures = response.json()['data']['attributes']['signature_count']
+
+    print('{} Scottish Votes'.format(scot_count))
+    print('{} English Votes'.format(eng_count))
+    print('{} Welsh Votes'.format(wales_count))
+    print('{} N.I Votes'.format(ni_count))
+
+    total_count_outside_scotland = outside_gb_count + eng_count + wales_count + ni_count
+
+    print('{} votes OUTSIDE Scotland'.format(total_count_outside_scotland))
 
 
 if __name__ == "__main__":
